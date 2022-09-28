bfcac6e7b14af7b6e1337645fad49bdd477442fb
maieulchevalier <maieul.chevalier@gmail.com>
2022-09-28 21:36:12 +0200

    implement ESlint rules #4

--- next.config.mjs
+++ next.config.mjs
@@ -13,12 +13,9 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
-});
-
-module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-};
+});
